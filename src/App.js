a07872f5c45d5c7e5e4d9709bf96dc7db8f4970a
hamidreza8968 <hamidreza.seydabadi68@gmail.com>
2023-07-02 20:40:06 +0200

    creating form of Form component.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
+
+const initialItems = [
+    {id: 1, description: "Passports", quantity: 2, packed: false},
+    {id: 2, description: "Socks", quantity: 12, packed: true},
+    {id: 3, description: "charger", quantity: 1, packed: false},
+];
 
 function App() {
     return (
@@ -18,16 +24,51 @@ function Logo() {
 
 
 function Form() {
+    const [description , setDescription] = useState("");
+    const [quantity , setQuantity] = useState(1);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        if (!description) return;
+
+        const newItem = {description , quantity , packed : false , id : Date.now()};
+
+        setDescription("");
+        setQuantity(1);
+    }
+    
     return (
-        <div className="add-form">
+        <form className="add-form" onSubmit={handleSubmit}>
             <h3>What do you need for your 😍 trip?</h3>
-        </div>
+            <select name="" id="" value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
+                {Array.from({length:20} , (_ , i) => i + 1).map(num => <option value={num} key={num}>{num}</option>)}
+            </select>
+            <input type="text" placeholder="Item..." value={description} onChange={(e) => setDescription(e.target.value)}/>
+            <button>Add</button>
+        </form>
     );
 }
 
 function PackingList() {
+    const renderedItems = initialItems.map((item) => <Item itemObj={item} key={item.id}/>);
     return (
-        <div className="list">LIST</div>
+        <div className="list">
+            <ul>{renderedItems}</ul>
+        </div>
+
+    )
+}
+
+function Item({itemObj}) {
+    return (
+        <li>
+            <span style={itemObj.packed ? {textDecoration:"line-through"} : {}}>
+                {itemObj.quantity} {itemObj.description}
+            </span>
+            <button>❌</button>
+        </li>
+
     )
 }
 
