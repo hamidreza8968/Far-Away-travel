e272fb3ac7065659e2e4137dcf09be92e65ef158
hamidreza8968 <hamidreza.seydabadi68@gmail.com>
2023-07-02 12:42:45 +0200

    building the layout of page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "./index.css";
-
-function App() {
-    return (
-        <div>Hello</div>
-    )
-}
+import App from "./App";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
