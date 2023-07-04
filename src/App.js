73bdb908ab6cfe2e3078166630862f06ee95e879
hamidreza8968 <hamidreza.seydabadi68@gmail.com>
2023-07-04 09:07:55 +0200

    adding the "toggle items" functionality

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import React, {useState} from "react";
 
-const initialItems = [
-    {id: 1, description: "Passports", quantity: 2, packed: false},
-    {id: 2, description: "Socks", quantity: 12, packed: true},
-    {id: 3, description: "charger", quantity: 1, packed: false},
-];
 
 function App() {
-    const [items , setItems] = useState([]);
+    const [items, setItems] = useState([]);
 
     function handleAddItems(item) {
-        setItems((items) => [...items , item]);
+        setItems((items) => [...items, item]);
     }
 
     function handleDeleteItems(id) {
-        setItems((items) => items.filter(item => item.id!==id))
+        setItems((items) => items.filter(item => item.id !== id))
+    }
+
+    function handleToggleItems(id) {
+        setItems((items) => items.map(item => item.id === id ? {...item , packed:!item.packed} : item))
     }
 
     return (
         <div className="app">
             <Logo/>
             <Form onAddItems={handleAddItems}/>
-            <PackingList items={items} onDeleteItems={handleDeleteItems}/>
+            <PackingList items={items} onDeleteItems={handleDeleteItems} onToggleItems={handleToggleItems}/>
             <Stats/>
         </div>
     )
@@ -34,8 +33,8 @@ function Logo() {
 
 
 function Form({onAddItems}) {
-    const [description , setDescription] = useState("");
-    const [quantity , setQuantity] = useState(1);
+    const [description, setDescription] = useState("");
+    const [quantity, setQuantity] = useState(1);
 
 
     function handleSubmit(e) {
@@ -43,28 +42,29 @@ function Form({onAddItems}) {
 
         if (!description) return;
 
-        const newItem = {description , quantity , packed : false , id : Date.now()};
+        const newItem = {description, quantity, packed: false, id: Date.now()};
 
         onAddItems(newItem);
 
         setDescription("");
         setQuantity(1);
     }
-    
+
     return (
         <form className="add-form" onSubmit={handleSubmit}>
             <h3>What do you need for your 😍 trip?</h3>
             <select name="" id="" value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
-                {Array.from({length:20} , (_ , i) => i + 1).map(num => <option value={num} key={num}>{num}</option>)}
+                {Array.from({length: 20}, (_, i) => i + 1).map(num => <option value={num} key={num}>{num}</option>)}
             </select>
-            <input type="text" placeholder="Item..." value={description} onChange={(e) => setDescription(e.target.value)}/>
+            <input type="text" placeholder="Item..." value={description}
+                   onChange={(e) => setDescription(e.target.value)}/>
             <button>Add</button>
         </form>
     );
 }
 
-function PackingList({items , onDeleteItems}) {
-    const renderedItems = items.map((item) => <Item item={item} onDeleteItems={onDeleteItems} key={item.id}/>);
+function PackingList({items , onDeleteItems , onToggleItems}) {
+    const renderedItems = items.map((item) => <Item item={item} onDeleteItems={onDeleteItems} onToggleItems={onToggleItems} key={item.id}/>);
     return (
         <div className="list">
             <ul>{renderedItems}</ul>
@@ -73,13 +73,15 @@ function PackingList({items , onDeleteItems}) {
     )
 }
 
-function Item({item , onDeleteItems}) {
+function Item({item , onDeleteItems , onToggleItems}) {
     return (
         <li>
-            <span style={item.packed ? {textDecoration:"line-through"} : {}}>
+            <input type="checkbox" value={item.packed} onChange={() => onToggleItems(item.id)
+            }/>
+            <span style={item.packed ? {textDecoration: "line-through"} : {}}>
                 {item.quantity} {item.description}
             </span>
-            <button onClick={()=>onDeleteItems(item.id)}>❌</button>
+            <button onClick={() => onDeleteItems(item.id)}>❌</button>
         </li>
     )
 }
