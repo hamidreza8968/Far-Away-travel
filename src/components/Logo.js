9c75d601a3bb0b95785611bf793355de98dc467a
hamidreza8968 <hamidreza.seydabadi68@gmail.com>
2023-07-04 12:14:05 +0200

    moving components to the separate files.

---
+++ src/components/Logo.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Logo() {
+    return <h1>🌴 Far Away 🧳</h1>
+}
+export default Logo;
---
+++ src/components/PackingList.js
@@ -0,0 +1,34 @@
+import React, {useState} from "react";
+import Item from "./Item";
+
+function PackingList({items, onDeleteItems, onToggleItems, onClearList}) {
+
+    const [sortBy, setSortBy] = useState("input");
+    let sortedItems;
+
+    if (sortBy === "input") sortedItems = items;
+    if (sortBy === "description") sortedItems = items.slice().sort((a, b) => a.description.localeCompare(b.description));
+    if (sortBy === "packed") sortedItems = items.slice().sort((a, b) => Number(a.packed) - Number(b.packed));
+
+    const renderedItems = sortedItems.map((item) => <Item item={item} onDeleteItems={onDeleteItems}
+                                                          onToggleItems={onToggleItems} key={item.id}/>);
+
+    return (
+        <div className="list">
+            <ul>{renderedItems}</ul>
+
+            <div className="actions">
+                <select name="" id="" value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+                    <option value="input">Sort by input order</option>
+                    <option value="description">Sort by description</option>
+                    <option value="packed">Sort by packed status</option>
+                </select>
+
+                <button onClick={onClearList}>Clear List</button>
+            </div>
+        </div>
+
+    )
+}
+
+export default PackingList;
